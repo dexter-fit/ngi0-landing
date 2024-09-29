4bfc54c4970709b735130839096e5165b84d7544
Juraj Lazúr <juraj51112@gmail.com>
2024-09-29 21:28:05 +0200

    Move description next to the image on large screens #9

--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -6,9 +6,17 @@ import {AssociatedProjectTemplate} from "./AssociatedProjectTemplate";
 
 const ProjectDescription = (props: ProjectDescriptionProps) => {
     return <>
-        {props?.image ? <div className="image-container">
-            <img src={props.image} alt="image" key={props.image}/>
-        </div> : <></>}
+        <h1 id={props.descriptionContent.anchor}>
+                {props.descriptionContent.header}
+        </h1>
+        <div className="image-content-wrapper">
+            {props?.image ? <img className="image" src={props.image} alt="image" key={props.image}/>: <></>}
+            {props.children}
+        </div>
+
+        <div className="flex gap-1 margin-btm">
+            {props.descriptionContent.links?.map(item => item)}
+        </div>
 
         {props.tags?.length ? <div className="content-wrapper">
             {props.tags}
@@ -18,15 +26,6 @@ const ProjectDescription = (props: ProjectDescriptionProps) => {
 
         <article key={props.descriptionContent.anchor}>
             <section>
-                <h1 id={props.descriptionContent.anchor}>
-                    {props.descriptionContent.header}
-                </h1>
-                <div>
-                    {props.children}
-                </div>
-                <div className="flex gap-1">
-                    {props.descriptionContent.links?.map(item => item)}
-                </div>
                 {props.descriptionContent.associatedProjects?.map(project => <AssociatedProjectTemplate
                     description={project.description}
                     carousel={project.carousel}
