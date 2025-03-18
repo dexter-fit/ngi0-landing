15672192722dcb478b373d7de20325748ca24f5b
Petr John <ijohn@fit.vut.cz>
2025-03-18 09:40:00 +0100

    Dossiers: Fix md images being too big

--- src/util/markdownToHtml.ts
+++ src/util/markdownToHtml.ts
@@ -2,7 +2,22 @@ import showdown from "showdown";
 
 // Function to convert Markdown to HTML
 const markdownToHtml = (markdown: string): string => {
-    const converter = new showdown.Converter({tables: true});
+    const classMap = {
+        img: 'md-image'
+    }
+
+    const bindings = Object.keys(classMap)
+        .map(key => ({
+            type: 'output',
+            regex: new RegExp(`<${key}(.*)>`, 'g'),
+            // @ts-ignore
+            replace: `<${key} class="${classMap[key]}" $1>`
+        }));
+
+    const converter = new showdown.Converter({
+        tables: true,
+        extensions: [...bindings]
+    });
 
     return converter.makeHtml(markdown);
 };
